56222fbe6e1f91cd57a058110da3285f9c24f047
Dan Bode <dan@puppetlabs.com>
2012-05-06 00:25:34 -0700

    Refactor nova networking
    Remove network specific params from nova class.
    These parameters have been removed b/c they are
    not used by all nova services.
    
    Also refactors the network implementation classes
    to be specifically for configuring the network modes.
    
    nova::network has been updated to configure networks
    as well as manage the network services.
    
    nova::network::flat - Removes explicit bridge creation.
    These manifests assume that nova will create the
    bridge that it needs so that the bridge does not
    have to be created explicitly.
    
    nova::network::flatdhcp Removes network service management.
    Removes flat_dhcp_start b/c it has been depreated.
    
    nova::network::vlan - add vlan_start and vlan_interface
    config params. Remove management of network service.
    
    Add bridge-utils to compute class.

--- manifests/network/flat.pp
+++ manifests/network/flat.pp
@@ -1,21 +1,20 @@
-# flat.pp
+# Configuration settings for nova flat network
+# ==Parameters
+# [flat_interface] Interface that flat network will use for bridging.
+# [flat_network_bridge] Name of bridge.
 class nova::network::flat (
-  $flat_network_bridge,
-  $configure_bridge = true,
-  $flat_network_bridge_ip,
-  $flat_network_bridge_netmask,
-  $enabled = "true"
+  $flat_interface,
+  $fixed_range,
+  $public_interface   = undef,
+  $flat_network_bridge = 'br100'
 ) {
 
-  class { 'nova::network':
-    enabled => $enabled,
+  nova_config {
+    'network_manager':     value => 'nova.network.manager.FlatDHCPManager';
+    'public_interface':    value => $public_interface;
+    'fixed_range':         value => $fixed_range;
+    'flat_interface':      value => $flat_interface;
+    'flat_network_bridge': value => $flat_network_bridge;
   }
 
-  # flatManager requires a network bridge be manually setup.
-  if $configure_bridge {
-    nova::network::bridge { $flat_network_bridge:
-      ip      => $flat_network_bridge_ip,
-      netmask => $flat_network_bridge_netmask,
-    }
-  }
 }
--- manifests/network/vlan.pp
+++ manifests/network/vlan.pp
@@ -1,8 +1,17 @@
 #vlan.pp
 class nova::network::vlan (
-  $enabled = true
+  $fixed_range,
+  $vlan_interface,
+  $public_interface = undef,
+  $vlan_start       = '300'
 ) {
-  class { 'nova::network':
-    enabled => $enabled,
+
+  nova_config {
+    'network_manager':  value => 'nova.network.manager.VlanManager';
+    'public_interface': value => $public_interface;
+    'fixed_range':      value => $fixed_range;
+    'vlan_interface':   value => $vlan_interface;
+    'vlan_start':       value => $vlan_start;
   }
+
 }
