3994c54d448d87a9e92f546105488c33a2255a46
Soren Hansen <sorhanse@cisco.com>
2012-05-10 18:51:42 +0000

    Rework db sync ordering

--- manifests/manage/admin.pp
+++ manifests/manage/admin.pp
@@ -1,11 +1,10 @@
 define nova::manage::admin {
 
   File['/etc/nova/nova.conf'] -> Nova_admin[$name]
-  Exec<| title == 'initial-db-sync' |> -> Nova_admin[$name]
+  Exec<| title == 'nova-db-sync' |> -> Nova_admin[$name]
 
   nova_admin{ $name:
     ensure => present,
-    provider => 'nova_manage',
-    notify => Exec["nova-db-sync"],
+    provider => 'nova_manage'
   }
 }
