d2783a31cb5d3b2e7519c28fae92c384d7dc9c86
Soren Hansen <sorhanse@cisco.com>
2012-05-11 11:23:55 -0700

    Fix syntax error

--- manifests/init.pp
+++ manifests/init.pp
@@ -122,7 +122,7 @@ class nova(
     command     => "/usr/bin/nova-manage db sync",
     require => [File['/etc/nova/nova.conf'],
 	            Package[python-nova],
-	            Nova_config['sql_connection']
+	            Nova_config['sql_connection'],
 	            Package[$::nova::params::common_package_name]],
 	refreshonly => "true",
   }
