58db05ef05c0ff9501c8759611045770981ae2ba
Edgar Magana <emagana@gmail.com>
2012-10-15 09:20:19 -0700

    Adding the puppet modules for Quantum

--- manifests/network/quantum.pp
+++ manifests/network/quantum.pp
@@ -3,6 +3,17 @@ class nova::network::quantum (
   $use_dhcp                = 'True',
   $public_interface        = undef,
   $quantum_connection_host = localhost,
+  $network_api_class	   = 'nova.network.quantumv2.api.API',
+  $quantum_url		   = 'http://127.0.0.1:9696',
+  $quantum_auth_strategy   = 'keystone',
+  $quantum_admin_tenant_name	= 'services',
+  $quantum_admin_username	= 'quantum',
+  $quantum_admin_password	= 'quantum',
+  $quantum_admin_auth_url	= 'http://127.0.0.1:35357/v2.0',
+  $libvirt_vif_driver	   = 'nova.virt.libvirt.vif.LibvirtOpenVswitchDriver',
+  $libvirt_use_virtio_for_bridges	= 'True',
+  $host		= 'compute',
+
 ) {
 
   if $public_interface {
@@ -14,8 +25,19 @@ class nova::network::quantum (
   }
 
   nova_config { 
-    'network_manager': value => 'nova.network.quantum.manager.QuantumManager';
-    'fixed_range': value => $fixed_range;
-    'quantum_use_dhcp': value => $use_dhcp;
+    'network_api_class': value => $network_api_class;
+    'quantum_url': value => $quantum_url;
+    'quantum_auth_strategy': value => $quantum_auth_strategy;
+    'quantum_admin_tenant_name': value => $quantum_admin_tenant_name;
+    'quantum_admin_username': value => $quantum_admin_username;
+    'quantum_admin_password': value => $quantum_admin_password;
+    'quantum_admin_auth_url': value => $quantum_admin_auth_url;
+}
+   if $host == 'compute' {
+    	 nova_config {
+	'libvirt_vif_driver': value => $libvirt_vif_driver;
+    'libvirt_use_virtio_for_bridges': value => $libvirt_use_virtio_for_bridges;
   }
+ }
+
 }
